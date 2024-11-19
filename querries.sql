b820ecac4e1764d513260e2f868c44b6b475e2bf
Spyros Sachmpazidis <123989999+Spyros2004@users.noreply.github.com>
2024-11-19 17:19:04 +0200

    Delete querries.sql

--- querries.sql
+++
@@ -1,7 +0,0 @@
-DECLARE @Username VARCHAR(50);
-SET @Username = 'john_doe';
-
-DECLARE @Password VARCHAR(50);
-SET @Password = 'password123';
-
-SELECT * FROM [dbo].[User] WHERE [Username] = @Username AND [Password] = @Password
