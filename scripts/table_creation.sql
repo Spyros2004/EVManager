5e331f0b66f3c48f69fbedd633a1110abbb0a58b
Spyros Sachmpazidis <123989999+Spyros2004@users.noreply.github.com>
2024-11-26 15:32:20 +0200

    Update table_creation.sql

--- scripts/table_creation.sql
+++ scripts/table_creation.sql
@@ -13,7 +13,7 @@ CREATE TABLE [dbo].[User]
     [Username] VARCHAR(50) NOT NULL, 
     [Email] VARCHAR(100) NOT NULL, 
     [Password] VARCHAR(255) NOT NULL, 
-    [User_Type] VARCHAR(20) NOT NULL CHECK ([User_Type] IN ('Admin', 'TOM', 'AA', 'User', 'Applicant')), 
+    [User_Type] VARCHAR(20) NOT NULL CHECK ([User_Type] IN ('Admin', 'TOM', 'AA', 'Applicant')), 
     [Status] VARCHAR(20) NOT NULL CHECK ([Status] IN ('pending', 'approved', 'suspended', 'deactivated', 'rejected')), 
     UNIQUE ([Username]),
     UNIQUE ([Email]),
