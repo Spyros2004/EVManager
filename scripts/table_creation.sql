09729502f5e4953de8c88b8a63c9cf6636b7beb4
Spyros Sachmpazidis <123989999+Spyros2004@users.noreply.github.com>
2024-11-27 20:55:29 +0200

    Update table_creation.sql

--- scripts/table_creation.sql
+++ scripts/table_creation.sql
@@ -62,11 +62,9 @@ CREATE TABLE [dbo].[Application]
 (
     [Application_ID] INT NOT NULL IDENTITY(1,1),
     [Application_Date] DATE NOT NULL DEFAULT GETDATE(),
-    [Discarder_Car_LPN] CHAR(6) DEFAULT NULL, 
     [Current_Status] VARCHAR(20) NOT NULL CHECK ([Current_Status] IN ('pending', 'approved', 'rejected', 'under_review', 'in_progress')) DEFAULT 'pending', 
     [Applicant_ID] INT NOT NULL,
     [Category_Number] INT NOT NULL,
-    UNIQUE ([Discarder_Car_LPN]),
     CONSTRAINT [PK_APPLICATION] PRIMARY KEY ([Application_ID] ASC)
 )
 
