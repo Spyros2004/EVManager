6ec13552d134875ceb777f9e0e12c61957a27295
Spyros Sachmpazidis <123989999+Spyros2004@users.noreply.github.com>
2024-11-29 21:47:46 +0200

    Update table_creation.sql

--- scripts/table_creation.sql
+++ scripts/table_creation.sql
@@ -64,7 +64,7 @@ CREATE TABLE [dbo].[Application]
 (
     [Application_ID] INT NOT NULL IDENTITY(1,1),
     [Application_Date] DATE NOT NULL DEFAULT GETDATE(),
-    [Current_Status] VARCHAR(20) NOT NULL CHECK ([Current_Status] IN ('pending', 'approved', 'rejected', 'under_review', 'in_progress')) DEFAULT 'pending', 
+    [Current_Status] VARCHAR(20) NOT NULL CHECK ([Current_Status] IN ('active', 'approved', 'rejected', 'under_review', 'in_progress')) DEFAULT 'pending', 
     [License_Plate] CHAR(6),
     [Applicant_ID] INT NOT NULL,
     [Category_Number] INT NOT NULL,
