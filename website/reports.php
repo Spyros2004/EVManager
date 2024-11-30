bcd8878cebbac22a7ba6f840f7f5ac34d6612038
Spyros Sachmpazidis <123989999+Spyros2004@users.noreply.github.com>
2024-11-30 21:16:46 +0200

    Update reports.php

--- website/reports.php
+++ website/reports.php
@@ -1,1 +1,16 @@
+<?php
+// Start the session to check if the user is logged in
+session_start();
+if (!isset($_SESSION['SessionID'])) {
+    // Redirect to login page if the session is not set
+    header("Location: login.php");
+    exit();
+}
 
+// Check if the user is authorized to access this page (Admin only)
+if ($_SESSION['UserTypeNumber'] != 1) {
+    // Redirect unauthorized users to the login page
+    header("Location: login.php");
+    exit();
+}
+php?>
