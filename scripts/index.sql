5d00f7eb50c8a09e3be1803c84cd92bf75a4b67b
Spyros Sachmpazidis <123989999+Spyros2004@users.noreply.github.com>
2024-12-04 21:22:52 +0200

    Update index.sql

--- scripts/index.sql
+++ scripts/index.sql
@@ -1,1 +1,10 @@
+--CREATE THE INDEXES
+CREATE UNIQUE NONCLUSTERED INDEX IX_Application_TrackingNumber ON Application (Tracking_Number);
 
+CREATE INDEX IX_Application_Status_Category ON Application (Current_Status, Category_Number);
+
+CREATE INDEX IX_Application_ApplicationDate ON Application (Application_Date);
+
+CREATE INDEX IX_Applicant_UserID_CompanyPrivate ON Applicant (User_ID, Company_Private);
+
+CREATE INDEX IX_Application_Status_Date ON Application (Current_Status, Application_Date);
