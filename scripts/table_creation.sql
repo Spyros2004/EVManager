d99904e81bb81327d3022fb80b2e5cad42ed63ff
Spyros Sachmpazidis <123989999+Spyros2004@users.noreply.github.com>
2024-12-04 18:28:52 +0200

    Update table_creation.sql

--- scripts/table_creation.sql
+++ scripts/table_creation.sql
@@ -120,12 +120,15 @@ CREATE TABLE [dbo].[User_Session]
 )
 
 --CREATE THE INDEXES
-CREATE UNIQUE NONCLUSTERED INDEX IX_Application_TrackingNumber 
-ON Application (Tracking_Number);
+CREATE UNIQUE NONCLUSTERED INDEX IX_Application_TrackingNumber ON Application (Tracking_Number);
 
-CREATE INDEX IX_Application_Status_Category 
-ON Application (Current_Status, Category_Number);
+CREATE INDEX IX_Application_Status_Category ON Application (Current_Status, Category_Number);
 
+CREATE INDEX IX_Application_ApplicationDate ON Application (Application_Date);
+
+CREATE INDEX IX_Applicant_UserID_CompanyPrivate ON Applicant (User_ID, Company_Private);
+
+CREATE INDEX IX_Application_Status_Date ON Application (Current_Status, Application_Date);
 
 --CREATE FOREIGN KEY CONSTRAINTS
 ALTER TABLE [dbo].[Discarded_Car] WITH CHECK ADD CONSTRAINT [FK_DISCARDED_CAR_APPLICATION]
